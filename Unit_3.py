c5e302bbbf10f5564b3bc052ac1ea78346bdd2e0
Gorshkov Maxim <maximdavinchi@gmail.com>
2021-10-14 23:25:53 +0300

    first commit

---
+++ Unit_3.py
@@ -0,0 +1,27 @@
+def counter(txt):
+    line_list = list()
+    for file in txt:
+        with open(file, encoding='utf-8') as f:
+            lines = f.readlines()
+            line_dict = {'lines': len(lines), 'file': file}
+            line_list.append(line_dict)
+    low_high_list = sorted(line_list, key=lambda i: i['lines'])
+    return low_high_list
+
+
+def resulting():
+    with open('../lessons_6_2/result.txt', 'w') as fw:
+        low_high_list = counter(documents)
+        for doc in low_high_list:
+            file_str = f'{doc["file"]}\n'
+            fw.write(file_str)
+            line_str = f'{doc["lines"]}\n'
+            fw.write(line_str)
+            fw.write('\n')
+    return
+
+
+if __name__ == '__main__':
+    documents = ['1.txt', '2.txt', '3.txt']
+    print(counter(documents))
+    print(resulting())
